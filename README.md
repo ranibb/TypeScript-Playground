e1b9235b02da4e71c74424132907fe790b985b24
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-08 12:41:50 +0300

    Classes and Object Oriented Design - Initial Comiit

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # TypeScript Playground
 
 * Run `npm start` for TypeScript execution and REPL (read–eval–print loop).
+
+# Classes and Object Oriented Design
+
+We focus on two main object orientated programming concepts: Data encapsulation and inheritance.
+
+## Data Encapsulation by use of Access Control on Class members
+
+We could access all class members from outside without restrictions. However, in many cases this is not what we want. For some variables we may want to prevent access from outside or at least we want to grant readonly access. To control access to the class members we have two basic strategies: Either we use access modifiers or alternatively Getter and Setter methods.
+
+### public/private/protected modifiers (access modifiers strategy)
+* If you don't use any access modifiers, TypeScript assumes public as default. That means the members can be freely accessed and modified from outside the class with the "." notation.
+* In order to prevent any access from outside the class we have to explicitly add the modifier private in front of the property.
+* And if you want to allow access from sub classes that inherit from our class but no access from outside we use the protected keyword.
+
+### readonly modifier (access modifiers strategy)
+* In case we want to allow only read access from outside that prevent modification, we use the modifier readonly.
+
+### Getter and Setter functions (Getter and Setter methods startegy)
+* To allow more fine grain control over member access we use Getter and Setter methods.
+* When using Getters and Setters, you first set all your members variables to private and that provide specific methods to read this data with a getter and modify the data by our setter.
+
+## Class Inheritance for building class hierarchies
+
+### Understanding Inheritance
+
+### Abstract Classes as base classes in class hierarchies that can't be instantiated them selves
+
--- app.ts
+++ app.ts
@@ -1,1 +1,68 @@
-console.log();
+interface Printable {
+    toString(): string
+}
+
+class Point implements Printable {
+    static origin = new Point(0,0);
+    constructor(public x: number, public y: number) {
+    }
+    distanceTo(otherPoint : Point) {
+        return Math.sqrt( Math.pow(otherPoint.x - this.x,2) + Math.pow(otherPoint.y - this.y,2) )
+    }
+    toString() {
+        return `Point: (${this.x}, ${this.y})`
+    }
+}
+
+function print(source: Printable) {
+    console.log(source.toString());
+}
+
+const homePosition : Point = new Point(0,0);
+console.log(homePosition);
+
+print(homePosition);
+console.log(homePosition.distanceTo(new Point(-1,-1)));
+
+/* The Trail Class has an instance property coordinates storing a sequence of points representing the trail.*/
+class Trail {
+   
+    coordinates: Point[] = [] // A sequence of points representing the trail
+
+    /* A constructor without parameters that just initializes an empty trail. That is a trail with no points. */
+    constructor() {
+        this.coordinates = [];
+    }
+
+    // A method for adding new points to the trail.
+    add(point: Point) : Trail {
+        this.coordinates.push(point);
+        return this; // returning the resulting trail instance.
+    }
+
+    // A method to return back the total distance of the whole trail.
+    totalDistance() : number {
+
+        /* The total distance is a sum of all distances between the points. So, we define a total variable. */
+        let total = 0;
+
+        /* Start with index 1 (that is the 2nd point) to add up the distances between points. */
+        for (let index = 1; index < this.coordinates.length; index++) {
+
+            /* So, the loop computes the sum of the distance from the 2nd point to the 1st point, The 3rd 
+            point to the 2nd point, and so on. */
+
+            total += this.coordinates[index].distanceTo(this.coordinates[index - 1])
+        }
+
+        return total;
+    }
+}
+
+let trail = new Trail();
+trail.add(homePosition);
+trail.add(new Point(0,1));
+trail.add(new Point(1,1));
+trail.add(homePosition);
+
+console.log(trail.totalDistance());
