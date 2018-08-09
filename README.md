f9af6d6694f3d72f53956140caa2f66f43c1d879
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-09 09:24:17 +0300

    Enumerations - Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # TypeScript Playground
 
 * Run `npm start` for TypeScript execution and REPL (read–eval–print loop).
+
+# Enumerations
+
+Basically, an Enumeration is a collection of constants that logically belong together and define such a type of a categorical variable that can take on a value from predefined set of admissible values. For instance, a person agenda, academic title or occupation could be of a type defined by Enumeration.
+
+## Types of Enumerations
+
+### Numeric Enumerations (Run-Time)
+
+    enum TaskStatus {
+        notStarted = 1,
+        inProgress,
+        finished
+    }
+
+### String Enumerations (Run-Time)
+
+    enum Department {
+        IT = "IT Department",
+        sales = "Sales Department",
+        management = "Management"
+    }
+
+### Constant Enumerations (Compile-Time)
+
+    const enum AgeGroup {
+        under20 = 1,
+        between20And30,
+        between30And40,
+        ....
+    }
+
+Enumerations have a different behavior when they are defined as constants. A constant enumeration exists only at compile time and replaced by its raw values in run time. Whereas other enumerations are run time constructs and allow run time functionality.
+
+## Working with Enumerations
+
+### Enumeration as a one-to-one mapping
+We will see that an Enumeration can be viewed as a one-to-one correspondent and you will learn how a Lookup as reversed lookup is done in case of non-constant Enumerations 
+
+### Evaluation of Enumeration with switch
+we will study the typical case where an Enumeration is used together with a switch statement to distinguish the cases predefined by the enumeration.
--- app.ts
+++ app.ts
@@ -1,1 +1,43 @@
-console.log();
+/* Let us assume we want model a rating type that has five categories: Poor, Fair, Average, Good and Excellent. 
+The ratings could correspond to the stars, from 1 star to 5 stars. Hovering over the rating members, we observe 
+that TypeScript automatically assign the values 0,1,2,3,4 to our entries. So, by default, an enum is
+interpreted as a numeric enum staring with 0 for the first entry, 1 for the next and so on. If you want to 
+change the numbers, let us say to 1,2,3,4 and 5, we can assign these values ourselves, note in such a case 
+where we only want to change the start value and count up from there, setting just the start value is enough. */
+enum Rating {
+    poor = 1,
+    fair,
+    average,
+    good,
+    excellent
+}
+
+/* We could also define an interface Rated that is implemented by all objects that have a rating of the type 
+Rating, which just defined by enumeration */
+interface Rated {
+    rating: Rating
+}
+
+/* With help of the enum and the interface, we could next define a function averageRating that takes in an 
+array of rated objects as input and returns the average rating expressed by number from 1 and 5.*/
+function averageRating(items : Rated[]) : number {
+    let total = 0; // We sum up all the ratings in a variable total.
+    for (let item of items) { // An array iteration
+        total += item.rating;
+    }
+    return total/items.length; // Divide total by the number of items to compute an average rating.
+}
+
+let items = [
+    {
+        rating: Rating.average
+    },
+    {
+        rating: Rating.good
+    },
+    {
+        rating: Rating.good
+    }
+]
+
+console.log(averageRating(items));
