1d23e2446274963da2fd626ee45ed482b9c55fe7
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-09 18:05:26 +0300

    Enumeration as a one-to-one mapping and Evaluation of Enumeration with switch

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Basically, an Enumeration is a collection of constants that logically belong tog
 ## Types of Enumerations
 
 ### Numeric Enumerations (Run-Time)
+by default, an enum is interpreted as a numeric enum staring with 0 for the first entry, 1 for the next and so on. If you want to change the numbers, let us say to 1,2,3,4 and 5, we can assign these values ourselves, note in such a case where we only want to change the start value and count up from there, setting just the start value is enough.
 
     enum TaskStatus {
         notStarted = 1,
@@ -17,6 +18,7 @@ Basically, an Enumeration is a collection of constants that logically belong tog
     }
 
 ### String Enumerations (Run-Time)
+Unlink the numeric enums, string enums don't have the auto increment behavior. Consequently, we have to assign each numeration case explicitly. One difference between numeric and string enumerations is that auto completion is not available for string enums.
 
     enum Department {
         IT = "IT Department",
@@ -35,10 +37,16 @@ Basically, an Enumeration is a collection of constants that logically belong tog
 
 Enumerations have a different behavior when they are defined as constants. A constant enumeration exists only at compile time and replaced by its raw values in run time. Whereas other enumerations are run time constructs and allow run time functionality.
 
+When we need some Run-Time functionality like lookup or reversed lookup we don't use constant. However, if you just need a Compile-Time support of an enumeration, it is ok to declare the enum constant and it’s also more efficient.
+
 ## Working with Enumerations
 
 ### Enumeration as a one-to-one mapping
-We will see that an Enumeration can be viewed as a one-to-one correspondent and you will learn how a Lookup as reversed lookup is done in case of non-constant Enumerations 
+We will see that an Enumeration can be viewed as a one-to-one correspondent and you will learn how a Lookup and a reversed lookup is done in case of non-constant Enumerations.
+
+The term constant and non-constant are not self-explanatory in case of an enum. The best way to find out about the difference between enum's declared constant and other enums is to run the TypeScript complier and compare the difference in a transpiled JavaScript. For any non-constant enum a lookup object is created, that maps the enum members to the raw values and that can be used at Run-Time for lookups or eventually revered lookups.
+
+If you run the TypeScript complier and compare the JavaScript constructs of the Numeric enum to string enum, we should notice a slight difference. In case of the numeric enums, a one-to-one mapping is implemented that allow lookups and revers lookups. However, in case of the string enum, just a lookup is available.
 
 ### Evaluation of Enumeration with switch
-we will study the typical case where an Enumeration is used together with a switch statement to distinguish the cases predefined by the enumeration.
+We will study the typical case where an Enumeration is used together with a switch statement to distinguish the cases predefined by the enumeration.
--- app.ts
+++ app.ts
@@ -1,9 +1,6 @@
-/* Let us assume we want model a rating type that has five categories: Poor, Fair, Average, Good and Excellent. 
-The ratings could correspond to the stars, from 1 star to 5 stars. Hovering over the rating members, we observe 
-that TypeScript automatically assign the values 0,1,2,3,4 to our entries. So, by default, an enum is
-interpreted as a numeric enum staring with 0 for the first entry, 1 for the next and so on. If you want to 
-change the numbers, let us say to 1,2,3,4 and 5, we can assign these values ourselves, note in such a case 
-where we only want to change the start value and count up from there, setting just the start value is enough. */
+/* Let us assume we want to model a rating type that has five categories: Poor, Fair, Average, Good and 
+Excellent. The ratings could correspond to the stars, from 1 star to 5 stars. Hovering over the rating members, 
+we observe that TypeScript automatically assign the values 0,1,2,3,4 to our entries. */
 enum Rating {
     poor = 1,
     fair,
@@ -11,6 +8,12 @@ enum Rating {
     good,
     excellent
 }
+
+/* To lookup the raw value corresponding to the Rating member average, we write enum name followed by square 
+brackets followed by the member name as a string, in our case "average" */
+console.log(Rating["average"]);
+/* The revers lookup we do the same, just instead of the member name we type the raw value */
+console.log(Rating[3]); 
 
 /* We could also define an interface Rated that is implemented by all objects that have a rating of the type 
 Rating, which just defined by enumeration */
@@ -28,6 +31,39 @@ function averageRating(items : Rated[]) : number {
     return total/items.length; // Divide total by the number of items to compute an average rating.
 }
 
+/* Example on how enumerations are evaluated with a switch control flow. For that let us write a function 
+countsByRating that takes a Rated items array like our first function but returns an array of numbers where 
+the elements with index 1 to 5 represent the numbers of the corresponding rating category from poor to 
+excellent. */
+function countsByRating(items : Rated[]) : number[] {
+
+    // Initialize an array with zeroes
+    let counts: number[] = [0, 0, 0, 0, 0, 0];
+    // Use the array iteration like before
+    for (let item of items) {
+        /* With a switch statement to decide which rating category to increment. For example, in case of poor 
+        rating we increase the corresponding count by 1 */
+        switch(item.rating) {
+            case Rating.poor:
+                counts[Rating.poor]++;
+                break;
+            case Rating.fair:
+                counts[Rating.fair]++;
+                break;
+            case Rating.average:
+                counts[Rating.average]++;
+                break;
+            case Rating.good:
+                counts[Rating.good]++;
+                break;
+            case Rating.excellent:
+                counts[Rating.excellent]++;
+                break;
+        }
+    }
+    return counts;
+}
+
 let items = [
     {
         rating: Rating.average
@@ -41,3 +77,21 @@ let items = [
 ]
 
 console.log(averageRating(items));
+console.log(countsByRating(items));
+
+/* Let us say that we need to deal with food data and we are required to add dietary information like gluten 
+free or vegetarian. For that we could define enumeration dietary info that has a member’s gluten free, diary free 
+and vegetarian. In each case, we assign a string expression that could be used for example in some web 
+template */
+enum DietaryInfo {
+    glutenFree = "gluten-free",
+    diaryFree ="diary-free",
+    vegetarian = "vegetarian",
+    vegan = "vegan"
+}
+
+/* To lookup the raw value corresponding to the DietaryInfo member glutenFree, we write enum name followed by 
+square brackets followed by the member name as a string, in our case "glutenFree" */
+console.log(DietaryInfo["glutenFree"]);
+/* The revers lookup for a string enum is not available. And if you check what is returned if you try: */
+console.log(DietaryInfo["gluten-free"]); // returns Undefined
