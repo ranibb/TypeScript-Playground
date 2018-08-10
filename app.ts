f91a86447d3d8eb82a32941ae98567e7cd06667b
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-10 13:09:35 +0300

    Replace a type by any to make this code generic for all arbitrary types.

--- app.ts
+++ app.ts
@@ -28,10 +28,14 @@ let sales : Sales[] = [
 ]
 
 /* A function that find the maximum sales. It takes salesData as an argument and returns a sales object back 
-that represents the sales on a list with maximum amount */
-function findMaximum(salesData: Sales[]) : Sales {
-    let max = salesData[0]; // Initially set the maximum to the first entry.
-    for (let element of salesData) {
+that represents the sales on a list with maximum amount.
+
+In a first step we can try to make this code generic for all arbitrary types so we can find maximum entries 
+not just for sales but any other data we put inside. One possible solution would be to replace a type by any. */
+
+function findMaximum(data: any[]) : any { // For clarity chnage also the parameter name to data
+    let max = data[0]; // Initially set the maximum to the first entry.
+    for (let element of data) {
         if (element.amount > max.amount) {
             max = element;
         }
@@ -40,5 +44,5 @@ function findMaximum(salesData: Sales[]) : Sales {
 }
 
 console.log(findMaximum(sales));
-/* Observe that this function is quite restricted in its use since its only work for data of the Sales type 
-and assumes also that we have an amount variable on the list. */
+/* The example still works but we lose all type safety. Independent of what you put inside we always get back 
+an object of type any. */
