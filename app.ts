457dce1f15213168f7c5e7677db243fdf9c3803b
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-15 11:05:08 +0300

    We studied the second and third variants of writing asynchronous functions: promises and their applications.

--- app.ts
+++ app.ts
@@ -51,3 +51,61 @@ function stringFromFileAsync(filename: string, callabck: (data: string) => void)
 /* To test the asyncronus function, provide "input.txt" as a first parameter and a call back wrtitten as an 
 arrow function as a second parameter. In our case, we just log the resulting data to the console. */
 stringFromFileAsync("input.txt", (data) => console.log(data));
+
+/* So for we studied the first variant of writing asynchronous functions by help of callbacks. For the second 
+and third variant we use promises instead of callbacks. In order to distinguish the function name, we call our 
+function stringFromFileAsyncP for promise and go back to the original function notation with just one 
+parameter; the filename, and with a return value. But the return value is not a plain string but a promise of 
+a string. So, using this function doesn't give us a string back, instead we get a promise that in the future 
+we will receive a string value. Now within our function we return a new promise object; we just define by a 
+function with two parameters: resolve and reject. With these parameters we determine when to reject the 
+promise with an error and when to resolve the promise with the string value. For the implementation we use 
+again, a node function readFile which works the traditional way with callbacks. In the error case we reject 
+our promise with an error and in the success case we resolve it with a string content. This is the way to 
+define an asynchronous function with promises. */
+
+function stringFromFileAsyncP(filename: string) : Promise<string> {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, (error, data) => {
+            if(error) {
+                reject(error)
+            }
+            let content = data.toString("UTF8");
+            resolve(content);
+        });
+    });
+}
+
+/* For the application we have two options/variants:
+
+The first is to use the .catch().then() syntax that makes use of the two methods for handling the reject and 
+the resolve cases. Thus, after stringFromFileAsyncP with the filename as parameter we first add a catch() 
+method that takes a handler with the error as parameter and in this reject case we just log the error message. 
+Then to return the promise again we continue by adding the then() with a handler providing the string data 
+promise. Here we also just log the string value. */
+stringFromFileAsyncP("input.txt")
+    .catch((error) => {
+        console.log(error)
+    })
+    .then((data) => {
+        console.log(data)
+    })
+
+/* For the application of promises since node version 7.6 we have also a second option/variant. The 
+async-await pattern. For that define a function printFileConent and add the "async" keyword in front of the
+"function" keyword. Within the function we then use the traditional try-catch-syntax for error handling like 
+in the synchronous case. In the try block we extract the string value by using the keyword "await" in front of 
+the promise returned by our function and then log the data. In the catch block we log the error message */
+async function printFileConent(filename){
+    try {
+        let data = await stringFromFileAsyncP(filename);
+        console.log(data);   
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+/* For a test invoke this method with a file as input*/
+printFileConent("input.txt")
+/* As you can observe the advantage of async-await pattern is that we might use a syntax intuitive and similar 
+to the synchronous case. */
